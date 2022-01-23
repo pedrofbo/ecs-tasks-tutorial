86078d338be6d0a3bea1d40383e9bcce6d146103
Pedro Brito <pedrofelipebo@gmail.com>
2022-01-22 23:22:38 -0300

    user_data full setup working

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -70,9 +70,13 @@ apt-get install -y python3-pip python3-virtualenv
 virtualenv /home/ubuntu/.disk_monitor/.env
 source /home/ubuntu/.disk_monitor/.env/bin/activate
 pip3 install -r /home/ubuntu/.disk_monitor/disk_monitor/requirements.txt
+mkdir -p /home/ubuntu/.aws
+echo -e "[default]\nregion = us-east-1" >> /home/ubuntu/.aws/config
 echo -e \
   '{\n\t"threshold": 0.2,\n\t"sns_topic": "${aws_sns_topic.disk_monitor.arn}",\n\t"instance_name":"${var.project_name}-worker"\n}' \
   >> /home/ubuntu/.disk_monitor/disk_monitor/config.json
+echo "* * * * * cd /home/ubuntu/.disk_monitor/disk_monitor/; . ../.env/bin/activate; python monitor.py -c config.json" >> /tmp/disk_monitor
+sudo -u ubuntu bash -c "crontab /tmp/disk_monitor"
 EOF
   root_block_device {
     delete_on_termination = true
--- terraform/network.tf
+++ terraform/network.tf
@@ -14,6 +14,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "disk_monitor"
   }
